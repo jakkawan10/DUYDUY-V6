bb9b08887a645148f3500992a4c92d63b365e2ef
jakawan <duyduy2521@gmail.com>
2025-07-22 14:03:54 +0700

    feat: add homepage at app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+// app/page.tsx
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold text-center">สวัสดีจากแอป DUYDUY 🎉</h1>
+    </main>
+  );
+}
